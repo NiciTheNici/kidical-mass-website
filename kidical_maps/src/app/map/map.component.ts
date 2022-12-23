4f96a5723efc7e196b8eccdaac289277598a7a57
NiciTheNici <nicolasvontobel@protonmail.ch>
2022-12-23 22:21:47 +0100

    add leaflet map from zurich

---
+++ kidical_maps/src/app/map/map.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  private map!: L.Map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [47.3727, 8.5419],
+      zoom: 13
+    });
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+}
