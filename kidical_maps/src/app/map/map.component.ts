78aefb0d30265660ac0e3cfc44b8c0295504eca9
NiciTheNici <nicolasvontobel@protonmail.ch>
2022-12-23 23:06:58 +0100

    add bürkliplatz marker

--- kidical_maps/src/app/map/map.component.ts
+++ kidical_maps/src/app/map/map.component.ts
@@ -9,19 +9,36 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {
 
   private map!: L.Map;
+  private markers: L.Marker[] = [];
+  private icon: L.Icon = L.icon({
+    iconUrl: 'https://whatemoji.org/wp-content/uploads/2020/07/Party-Popper-Emoji.png',
+    iconSize: [50, 50],
+  });
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [47.3727, 8.5419],
       zoom: 13
     });
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
+
+    L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      { attribution: '© OpenStreetMap contributors' }
+    ).addTo(this.map);
+
+    this.markers.push(
+      L.marker(
+        [47.3672808, 8.5408271],
+        {
+          icon: this.icon,
+        }
+      ));
+
+    this.markers.map(marker => marker.addTo(this.map));
   }
 
   ngOnInit(): void {
     this.initMap();
   }
-
 }
+
