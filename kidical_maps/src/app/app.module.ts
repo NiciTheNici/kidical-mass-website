8b6dc86cb2d627f6dd2134848f0bca48eae17572
NiciTheNici <nicolasvontobel@protonmail.ch>
2022-12-26 00:45:17 +0100

    move entry to separate component

--- kidical_maps/src/app/app.module.ts
+++ kidical_maps/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { EntryComponent } from './entry/entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    EntryComponent,
   ],
   imports: [
     BrowserModule,
