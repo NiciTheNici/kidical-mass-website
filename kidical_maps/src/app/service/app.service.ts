99309df62bd1f2bfdca00f330f4f3114bc2e7614
NiciTheNici <nicolasvontobel@protonmail.ch>
2022-12-29 23:37:15 +0100

    move entries data to json file

--- kidical_maps/src/app/service/app.service.ts
+++ kidical_maps/src/app/service/app.service.ts
@@ -5,39 +5,9 @@ import { Entry } from '../entry/entry';
   providedIn: 'root'
 })
 export class AppService {
-  public entries: Entry[] = [
-    {
-      title: "Zurich",
-      description: "The kicical mass in zurich",
-      location: {
-        lat: 47.3672808,
-        lng: 8.5408271,
-      }
-    }, {
-      title: "St.Gallen",
-      description: "The kicical mass in St.Gallen",
-      location: {
-        lat: 47.42259,
-        lng: 9.37600,
-      }
-    }, {
-      title: "Baden",
-      description: "The kicical mass in Baden",
-      location: {
-        lat: 47.47105,
-        lng: 8.30761,
-      }
-    }, {
-      title: "Basel",
-      description: "The kicical mass in Basel",
-      location: {
-        lat: 46.19803,
-        lng: 6.14065,
-      }
-    },
-  ];
+
+  public entries: Entry[] = require("../entries.json");
 
   constructor() {
-
   }
 }
